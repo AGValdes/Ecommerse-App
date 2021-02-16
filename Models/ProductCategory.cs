e64fe4635c707b303fb3d49ce25bfacff19cb438
ameilia valdes <valdes.ameilia@gmail.com>
2021-02-15 17:10:56 -0800

    added scaffolding files for models and views, added a controller. Got the index page printing a list of all item categories

---
+++ Models/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce_App.Models
+{
+	public class ProductCategory
+	{
+		public int ProductId { get; set; }
+		public int CategoryId { get; set; }
+
+
+		public ProductCategory(Category category, Product product)
+		{
+			ProductId = product.Id;
+			CategoryId = category.Id;
+		}
+	}
+}
