dd17b67c1dada81acacb54209428fabda1baf60f
Overholtk <overholtkaya@gmail.com>
2021-02-16 16:46:04 -0800

    setup

---
+++ Auth/Models/DTO/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce_App.Auth.Models.DTO
+{
+    public class UserDTO
+    {
+        public string ID { get; set; }
+        public string Username { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
